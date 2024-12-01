a7c01b37a1591b0538812143bb657e568896a35d
nuttipol <nuttipol.r@gmail.com>
2024-12-01 15:48:30 +0700

    solve deploy error

--- server.js
+++ server.js
@@ -1,3 +1,43 @@
+// require('dotenv').config();
+
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cookieParser = require("cookie-parser");
+// const cors = require("cors");
+// const authRouter = require("./routes/auth/auth-routes");
+// const adminProductsRouter = require('./routes/admin/products-routes')
+
+// mongoose
+//   .connect(process.env.MONGO_URL)
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((error) => console.log(error));
+
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_BASE_URL,
+//     methods: ["GET", "POST", "DELETE", "PUT"],
+//     allowedHeaders: [
+//       "Content-Type",
+//       "Authorization",
+//       "Cache-Control",
+//       "Expires",
+//       "Pragma",
+//     ],
+//     credentials: true,
+//   })
+// );
+
+// app.use(cookieParser());
+// app.use(express.json());
+// app.use("/api/auth", authRouter);
+// app.use('/api/admin/products', adminProductsRouter)
+
+
+// app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
+
 require('dotenv').config();
 
 const express = require("express");
@@ -5,7 +45,7 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const authRouter = require("./routes/auth/auth-routes");
-const adminProductsRouter = require('./routes/admin/products-routes')
+const adminProductsRouter = require('./routes/admin/products-routes');
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -15,9 +55,20 @@ mongoose
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = [
+  process.env.CLIENT_BASE_URL, 
+  // 'http://localhost:5173'  // This should be included only for local testing.
+];
+
 app.use(
   cors({
-    origin: process.env.CLIENT_BASE_URL,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
@@ -30,10 +81,11 @@ app.use(
   })
 );
 
+app.options('*', cors());
+
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
-app.use('/api/admin/products', adminProductsRouter)
-
+app.use('/api/admin/products', adminProductsRouter);
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
